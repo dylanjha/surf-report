34ce56fe4a5222118b51854d009ff1615f0f54d7
Dylan Jhaveri <dylanjhaveri@gmail.com>
2014-09-11 00:05:02 -0700

    load UIImageVIews into scroll view with photos from instagram

--- Surf Report/ViewController.swift
+++ Surf Report/ViewController.swift
@@ -8,15 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         let manager = AFHTTPRequestOperationManager()
         manager.GET("https://api.instagram.com/v1/tags/SurferPhotos/media/recent?client_id=6ad531cd5fdc4b1aa78df742986bbef9",
             parameters: nil,
             success: { (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) in
-                println("JSON: " + responseObject.description)
+                if let data = responseObject.valueForKey("data") as? [AnyObject] {
+                    for var i = 0; i < data.count; i++ {
+                        let dataObj: AnyObject = data[i]
+                        if let imageURLString = dataObj.valueForKeyPath("images.standard_resolution.url") as? String {
+                            let imageData = NSData(contentsOfURL: NSURL(string: imageURLString))
+                            let imageView = UIImageView(image: UIImage(data: imageData))
+                            imageView.frame = CGRectMake(0, CGFloat(320*i), 320, 320)
+                            self.scrollView.addSubview(imageView)
+                        }
+                        
+                    }
+                }
             },
             failure: { (operation: AFHTTPRequestOperation!, error: NSError!) in
                 println("JSON: " + error.localizedDescription)
